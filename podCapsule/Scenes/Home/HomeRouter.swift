0bf85c538f407a45621c0d8ed46b5ed943e347d3
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-02 07:34:19 +0200

    added protocol functions

--- podCapsule/Scenes/Home/HomeRouter.swift
+++ podCapsule/Scenes/Home/HomeRouter.swift
@@ -9,17 +9,36 @@ import UIKit
 
 class HomeRouter{
     
+    var HomeView: UIViewController?
+    
     static func create() -> UIViewController {
         
         let view = HomeVC()
         let networkInteractor = HomeNetworkInteractor()
         let localInteractor = HomeLocalInteractor()
-        let presenter = HomePresenter(view: view, networkInteractor: networkInteractor, localInteractor: localInteractor)
+        let router = HomeRouter()
+        
+        let presenter = HomePresenter(view: view, networkInteractor: networkInteractor, localInteractor: localInteractor, router: router)
         
         view.presenter = presenter
         networkInteractor.presenter = presenter
         localInteractor.presenter = presenter
-        
+        router.HomeView = view
         return view
     }
+}
+
+extension HomeRouter: HomeViewRouter {
+    
+    func moveToPlayer(with episode: EpisodeObject) {
+        let playerVC = PlayerRouter.create(with: episode)
+        playerVC.modalPresentationStyle = .overFullScreen
+        HomeView?.present(playerVC, animated: true)
+    }
+    
+    func moveToPodcastDetails(with podcast: HomePodcastResponse) {
+        
+    }
+    
+    
 }
