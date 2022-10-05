e23ca7d8195a9ee9bb844b52ef4eff8dc87db8dc
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-05 06:37:46 +0200

    Created podcast details protocols

--- podCapsule/Scenes/PodcastDetails/PodcastDetailsProtocols.swift
+++ podCapsule/Scenes/PodcastDetails/PodcastDetailsProtocols.swift
@@ -5,13 +5,27 @@
 //  Created by mohamedSliem on 10/2/22.
 //
 
-import Foundation
+import UIKit
 
 
 protocol PodcastDetailsView: class {
     
     var presenter: PodcastDetailsViewPresenter? { get set }
     
+    func updateScrollViewHeight()
+    
+    func displayPodcastPoster(with url: URL?)
+    func displayPodcastTitle(title: String)
+    func displayPodcastPublisher(publisher: String)
+    func displayPodcastDescription(description: String)
+    func displayEpisodeNumber(number: String)
+    
+    func showLoader()
+    func hideLoader()
+    
+    func reloadEpisodesTableView()
+    
+    func showErrorAlert(title: String, message: String, actionTitle: String, actionHandler: @escaping ((UIAlertAction)?) -> ())
 }
 
 protocol PodcastDetailsViewPresenter: class {
@@ -22,17 +36,27 @@ protocol PodcastDetailsViewPresenter: class {
 
     init(view: PodcastDetailsView?, router: PodcastDetailsViewRouter?, interactor: PodcastDetailsInteractorInput?, podcast: PodcastObject)
     
+    func viewDidLoad()
+    
+    func tableViewCount() -> Int
+    func configureCell(at indexPath: Int)
     
 }
 
 protocol PodcastDetailsInteractorInput: class {
     
     var presenter: PodcastDetailsInteractorOutput? { get set }
+    
+    func getPodcastDetails(with id: String, sort: String)
+    
 }
 
 protocol PodcastDetailsInteractorOutput: class {
     
     var interactor: PodcastDetailsInteractorInput? { get set }
+    
+    func success(with podcastDetails: PodcastDetailsObject)
+    func failed(with error: Error)
 }
 
 protocol PodcastDetailsViewRouter: class {
