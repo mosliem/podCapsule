dcce1065b761312f778df01afc9127a117d7cacb
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-05 06:39:52 +0200

    Created podcast details view and its rendering functions

--- podCapsule/Scenes/PodcastDetails/View/PodcastDetailsVC.swift
+++ podCapsule/Scenes/PodcastDetails/View/PodcastDetailsVC.swift
@@ -8,13 +8,77 @@
 import UIKit
 
 class PodcastDetailsVC: UIViewController {
-
+    
     var presenter: PodcastDetailsViewPresenter?
-
+    
+    @IBOutlet weak var topView: UIView!
+    @IBOutlet weak var topViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var podcastTitleLabel: UILabel!
+    
+    @IBOutlet weak var podcastPosterShadowView: UIView!
+    @IBOutlet weak var podcastPosterImageView: UIImageView!
+    @IBOutlet weak var publisherNameLabel: UILabel!
+    @IBOutlet weak var episodesNumberLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    @IBOutlet weak var episodesTableView: UITableView!
+    
+    @IBOutlet weak var scrollView: UIView!
+    @IBOutlet weak var scrollViewHeightConstraint: NSLayoutConstraint!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+        let color = UIColor.clear
+        let appearnce = UINavigationBarAppearance()
+        
+        appearnce.backgroundColor = color
+        navigationController?.navigationBar.standardAppearance = appearnce
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .blue
+        
+        renderView()
+        presenter?.viewDidLoad()
     }
+    
+    private func renderView(){
+        configurePosterShadow()
+        roundPodcastPoster()
+        configureTopView()
+    }
+    
+    
+    private func configureTableView(){
+        
+        episodesTableView.delegate = self
+        episodesTableView.dataSource = self
+        
+        
+    }
+    
+    private func roundPodcastPoster(){
+        podcastPosterImageView.clipsToBounds = true
+        podcastPosterImageView.layer.cornerRadius = 20
+    }
+    
+    private func configurePosterShadow(){
+        podcastPosterShadowView.layer.cornerRadius = 20
+        podcastPosterShadowView.layer.shadowColor = UIColor.lightGray.cgColor
+        podcastPosterShadowView.layer.shadowOffset = CGSize(width: 0, height: 5)
+        podcastPosterShadowView.layer.shadowRadius = 10
+        podcastPosterShadowView.layer.shadowOpacity = 0.3
+    }
+    
+    private func configureTopView(){
+        
+        topView.layer.cornerRadius = 20
+        topView.layer.maskedCorners = [.layerMaxXMaxYCorner, .layerMinXMaxYCorner]
 
+        topView.layer.shadowColor = UIColor.lightGray.cgColor
+        topView.layer.shadowRadius = 15
+        topView.layer.shadowOffset = CGSize(width: 0, height: 10)
+        topView.layer.shadowOpacity = 0.4
+        
+    }
 }
