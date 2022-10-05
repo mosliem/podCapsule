36ab4f09d70e3edcd1265a9e2458d020bb18ff28
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-05 06:35:38 +0200

    Hanlded getting podcast details

--- podCapsule/Scenes/PodcastDetails/PodcastDetailsInteractor.swift
+++ podCapsule/Scenes/PodcastDetails/PodcastDetailsInteractor.swift
@@ -8,7 +8,23 @@
 import Foundation
 
 class PodcastDetailsInteractor: PodcastDetailsInteractorInput{
+ 
     var presenter: PodcastDetailsInteractorOutput?
+
     
-    
+    func getPodcastDetails(with id: String, sort: String){
+
+        let endpoint = PodcastDetailsRequest.getPodcastDetails(id: id, sort: sort)
+        
+        NetworkManger.shared.callRequest(objectType: PodcastDetailsObject.self, endpoint: endpoint) { [weak self](result) in
+            
+            switch result{
+                case .success(let podcastDetails):
+                    self?.presenter?.success(with: podcastDetails)
+                
+                case .failure(let error):
+                    self?.presenter?.failed(with: error)
+            }
+        }
+    }
 }
