6c19c1a9220a165d7fd904652da482d52a51cd79
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-05 06:38:43 +0200

    Setted Podcast Details request

--- podCapsule/Scenes/PodcastDetails/PodcastDetailsRequest.swift
+++ podCapsule/Scenes/PodcastDetails/PodcastDetailsRequest.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+
+enum PodcastDetailsRequest: Endpoint {
+    
+    case getPodcastDetails(id: String, sort: String)
+    
+    
+    var URLPath: String{
+        return "podcasts/"
+    }
+    
+    var parameters: Parameters{
+        
+        var parameters = defualtParameters
+        
+        switch self {
+        case.getPodcastDetails(id: let id, sort: let sort):
+            parameters = ["id": id,"sort": sort]
+        }
+        
+        return parameters
+    }
+    
+    var method: HTTPMethod{
+        return .GET
+    }
+    
+}
