85611533ee18167bba2e56e79cad695922d116a3
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-06 04:01:45 +0200

    Added realm list extension

--- podCapsule/Resourses/Extensions/RealmExtensions.swift
+++ podCapsule/Resourses/Extensions/RealmExtensions.swift
@@ -5,4 +5,14 @@
 //  Created by mohamedSliem on 10/6/22.
 //
 
-import Foundation
+import RealmSwift
+
+extension List {
+    
+    // Genertic function to convert Results list to an array of the same Results type
+    func toArray<T>(type: T.Type) -> [T] {
+        return compactMap {
+            $0 as? T
+        }
+    }
+}
