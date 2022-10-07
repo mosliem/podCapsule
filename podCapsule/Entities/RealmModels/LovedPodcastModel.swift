9e1e5d0625e734175eeefc362f022b808c91edc7
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-08 01:19:34 +0200

    Created loved podcast realm model

--- podCapsule/Entities/RealmModels/LovedPodcastModel.swift
+++ podCapsule/Entities/RealmModels/LovedPodcastModel.swift
@@ -5,4 +5,14 @@
 //  Created by mohamedSliem on 10/7/22.
 //
 
-import Foundation
+import RealmSwift
+
+class LovedPodcastModel: Object{
+    @Persisted(primaryKey: true) var id: String?
+    @Persisted var title: String
+    @Persisted var publisher: String
+    @Persisted var image: String?
+    @Persisted var info: String?
+    @Persisted var total_episodes: Int?
+    @Persisted var genre_ids: List<Int>
+}
