c814074d25c18e58ec589761ded2346d43132113
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-08 01:18:30 +0200

    Created local interactor with its functions

--- podCapsule/Scenes/PodcastDetails/PodcastDetailsLocalInteractor.swift
+++ podCapsule/Scenes/PodcastDetails/PodcastDetailsLocalInteractor.swift
@@ -5,4 +5,45 @@
 //  Created by mohamedSliem on 10/7/22.
 //
 
-import Foundation
+import RealmSwift
+
+class PodcastDetailsLocalInteractor: PodcastDetailsLocalInteractorInput{
+        
+    var presenter: PodcastDetailsLocalInteractorOutput?
+    
+    func addPodcastToLovedPodcast<T>(type: T.Type, object: T) {
+        
+        RealmManager.shared.add(object: object as! Object) {[weak self] (result) in
+            
+            switch result{
+                case .success(let success):
+                    self?.presenter?.success(with: success)
+                case .failure(let error):
+                    self?.presenter?.error(with: (error as! RealmError).errorMessage)
+            }
+        }
+    }
+    
+    func removePodcastFromLovedPodcast<T>(type: T.Type, id: String) {
+        
+        RealmManager.shared.deleteObject(with: id, type: type) { [weak self] (result) in
+            
+            switch result{
+                case .success(let success):
+                    self?.presenter?.success(with: success)
+                    
+                case .failure(let error):
+                    self?.presenter?.error(with: (error as! RealmError).errorMessage)
+            }
+        }
+    }
+
+    func isExistInLovedList(id: String) {
+        
+        RealmManager.shared.checkIfExist(with: id, type: LovedPodcastModel.self) { (exist) in
+            self.presenter?.existedInLovedList(existed: exist)
+        }
+
+    }
+
+}
