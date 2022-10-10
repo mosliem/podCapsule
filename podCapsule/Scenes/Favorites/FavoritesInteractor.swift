a3995a55f28173d136e346f10b376ba809672856
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-10 08:31:25 +0200

    Created local interactor of Favorites View

--- podCapsule/Scenes/Favorites/FavoritesInteractor.swift
+++ podCapsule/Scenes/Favorites/FavoritesInteractor.swift
@@ -5,11 +5,39 @@
 //  Created by mohamedSliem on 9/10/22.
 //
 
-import Foundation
+import RealmSwift
 
 class FavoritesInteractor: FavoritesInteractorInput{
     
     var presenter: FavoritesInteractorOutput?
     
+    
+    
+    func getFavoritePodcasts() {
+        
+        RealmManager.shared.retrieveAllObjects(LovedPodcastModel.self) { (result) in
+            
+            switch result{
+                case .success(let podcasts):
+                    self.presenter?.success(with: podcasts as! [LovedPodcastModel])
+                case .failure(let error):
+                    self.presenter?.failed(with: (error as! RealmError).errorMessage)
+            }
+        }
+    }
+    
+    func getFavoriteEpisodes() {
+        
+        RealmManager.shared.retrieveAllObjects(LovedEpisode.self) { (result) in
+            
+            switch result{
+                case .success(let episodes):
+                    self.presenter?.success(with: episodes as! [LovedEpisode])
+                case .failure(let error):
+                    self.presenter?.failed(with: (error as! RealmError).errorMessage)
+            }
+        }
+        
+    }
     
 }
