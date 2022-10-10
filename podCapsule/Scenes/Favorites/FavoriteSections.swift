c054232e0ffe23bc4eca1120d18c9cf365e2baa0
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-10 08:32:11 +0200

    Added Favorite Sections Handler

--- podCapsule/Scenes/Favorites/FavoriteSections.swift
+++ podCapsule/Scenes/Favorites/FavoriteSections.swift
@@ -6,3 +6,26 @@
 //
 
 import Foundation
+
+enum FavoriteSections: String, CaseIterable{
+    case Podcasts
+    case Episodes
+}
+
+struct FavoriteSectionsHandler{
+    
+    var sections = [FavoriteSections]()
+    
+    init() {
+        sections = []
+    }
+    mutating func append(section: String){
+        
+        guard let sectionName = (FavoriteSections.allCases.first{$0.rawValue == section}) else{
+            return
+        }
+        
+        sections.append(sectionName)
+    }
+    
+}
