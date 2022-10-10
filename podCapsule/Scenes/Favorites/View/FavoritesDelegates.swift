b66daf0b25a80545b1b8ca07c6cfac2a40bfc90a
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-10 08:28:52 +0200

    Added view protocols and delegation methods

--- podCapsule/Scenes/Favorites/View/FavoritesDelegates.swift
+++ podCapsule/Scenes/Favorites/View/FavoritesDelegates.swift
@@ -5,9 +5,60 @@
 //  Created by mohamedSliem on 9/10/22.
 //
 
-import Foundation
+import UIKit
 
 extension FavoritesVC: FavoritesView{
+    
+    func reloadFavoritesData() {
+        DispatchQueue.main.async {
+            self.favoritesCollectionView?.reloadData()
+        }
+    }
+    
+    func showLoader() {
+        self.showIndicator()
+    }
+    
+    func hideLoader() {
+        DispatchQueue.main.async {
+            self.hideIndicator()
+        }
+    }
+    
+    func appendPodcastSectionLayout() {
+        sectionsLayout?.append(createFavoritePodcastSectionLayout())
+    }
+    
+    func appendEpisodeSectionLayout() {
+        sectionsLayout?.append(createFavoriteEpisodeSectionLayout())
+    }
+    
+    func setCollectionSectionsLayout() {
+        favoritesCollectionView?.collectionViewLayout = configureCollectionViewSectionLayout()
+    }
+    
+    func showErrorAlert(title: String, message: String, actionTitle: String, actionHandler: @escaping (UIAlertAction?) -> ()) {
+        self.showAlertWithOk(alertTitle: title, message: message, actionTitle: actionTitle, actionHandler: actionHandler)
+    }
+    
+}
+
+extension FavoritesVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return presenter?.numberOfSection() ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return presenter?.numberOfItems(for: section) ?? 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+     
+        let cell = UICollectionViewCell()
+        presenter?.configureCell(cell: cell , type: (presenter?.type(for: indexPath.section))!)
+        return UICollectionViewCell()
+    }
     
     
 }
