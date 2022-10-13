cc526630f91ca06452ae0184c673a21f4b563ce2
Mohamed Sliem <52772674+mosliem@users.noreply.github.com>
2022-10-13 07:21:17 +0200

    Updated Favorites Interactor

--- podCapsule/Scenes/Favorites/FavoritesInteractor.swift
+++ podCapsule/Scenes/Favorites/FavoritesInteractor.swift
@@ -9,35 +9,35 @@ import RealmSwift
 
 class FavoritesInteractor: FavoritesInteractorInput{
     
-    var presenter: FavoritesInteractorOutput?
-    
-    
+    weak var presenter: FavoritesInteractorOutput?
     
     func getFavoritePodcasts() {
         
-        RealmManager.shared.retrieveAllObjects(LovedPodcastModel.self) { (result) in
+        RealmManager.shared.retrieveAllObjects(LovedPodcastModel.self) { [weak self] (result) in
             
             switch result{
                 case .success(let podcasts):
-                    self.presenter?.success(with: podcasts as! [LovedPodcastModel])
+                    self?.presenter?.success(with: podcasts as! [LovedPodcastModel])
+                    
                 case .failure(let error):
-                    self.presenter?.failed(with: (error as! RealmError).errorMessage)
+                    self?.presenter?.failed(with: (error as! RealmError).errorMessage)
             }
         }
     }
     
     func getFavoriteEpisodes() {
         
-        RealmManager.shared.retrieveAllObjects(LovedEpisode.self) { (result) in
+        RealmManager.shared.retrieveAllObjects(LovedEpisode.self) { [weak self] (result) in
             
             switch result{
+            
                 case .success(let episodes):
-                    self.presenter?.success(with: episodes as! [LovedEpisode])
+                    self?.presenter?.success(with: episodes as! [LovedEpisode])
+                    
                 case .failure(let error):
-                    self.presenter?.failed(with: (error as! RealmError).errorMessage)
+                    self?.presenter?.failed(with: (error as! RealmError).errorMessage)
             }
         }
-        
     }
     
 }
